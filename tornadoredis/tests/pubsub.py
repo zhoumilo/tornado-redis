640a85309f585b55847c51999fb4aab09df01454
leporo <high.slopes@gmail.com>
2012-11-14 10:30:32 +0200

    Fixed Pub/Sub tests.

--- tornadoredis/tests/pubsub.py
+++ tornadoredis/tests/pubsub.py
@@ -1,3 +1,4 @@
+from functools import partial
 from tornado import gen
 
 from redistest import RedisTestCase, async_test
@@ -21,9 +22,12 @@ class PubSubTestCase(RedisTestCase):
     def pause(self, timeout, callback=None):
         self.delayed(timeout, callback)
 
-    def _expect_messages(self, messages, expected_number):
+    def _expect_messages(self, messages, expected_number,
+                         subscribe_callback=None, callback=None):
         self._expected_messages = messages
         self._expected_number = expected_number
+        self._subscribe_callback = subscribe_callback
+        self._done_callback = callback
 
     def _handle_message(self, msg):
         self._message_count += 1
@@ -31,24 +35,34 @@ class PubSubTestCase(RedisTestCase):
         expected = self._expected_messages[msg.kind]
         self.assertEqual(msg.pattern, expected[0])
         self.assertEqual(msg.body, expected[1])
+        if msg.kind in ('subscribe', 'psubscribe'):
+            if self._subscribe_callback:
+                cb = self._subscribe_callback
+                self._subscribe_callback = None
+                cb(True)
         if self._message_count >= self._expected_number:
+            if self._done_callback:
+                self._done_callback(True)
             self.stop()
 
     @async_test
     @gen.engine
     def test_pub_sub(self):
-        yield gen.Task(self.client.subscribe, 'foo')
-        self.client.listen(self._handle_message)
         self._expect_messages({'subscribe': ('foo', 1),
                                'message': ('foo', 'bar'),
                                'unsubscribe': ('foo', 0)},
-                              3)
+                              3,
+                              subscribe_callback=(yield gen.Callback('sub')),
+                              callback=(yield gen.Callback('done')))
+        yield gen.Task(self.client.subscribe, 'foo')
+        self.client.listen(self._handle_message)
+        yield gen.Wait('sub')
         yield gen.Task(self.publisher.publish, 'foo', 'bar')
         yield gen.Task(self.publisher.publish, 'foo', 'bar')
         yield gen.Task(self.publisher.publish, 'foo', 'bar')
         yield gen.Task(self.client.unsubscribe, 'foo')
 
-        self.wait()
+        yield gen.Wait('done')
 
         self.assertEqual(self._message_count, 3)
         self.stop()
@@ -56,18 +70,21 @@ class PubSubTestCase(RedisTestCase):
     @async_test
     @gen.engine
     def test_pub_psub(self):
-        yield gen.Task(self.client.psubscribe, 'foo.*')
-        self.client.listen(self._handle_message)
         self._expect_messages({'psubscribe': ('foo.*', 1),
                                'pmessage': ('foo.*', 'bar'),
                                'punsubscribe': ('foo.*', 0),
                                'unsubscribe': ('foo.*', 1)},
-                              2)
+                              2,
+                              subscribe_callback=(yield gen.Callback('sub')),
+                              callback=(yield gen.Callback('done')))
+        yield gen.Task(self.client.psubscribe, 'foo.*')
+        self.client.listen(self._handle_message)
+        yield gen.Wait('sub')
         yield gen.Task(self.publisher.publish, 'foo.1', 'bar')
         yield gen.Task(self.publisher.publish, 'bar.1', 'zar')
         yield gen.Task(self.client.punsubscribe, 'foo.*')
 
-        self.wait()
+        yield gen.Wait('done')
 
         self.assertEqual(self._message_count, 2)
         self.stop()
