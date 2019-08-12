968f3093dc7de1bfb3979250c0f0d4eb5adb0427
jinyuanzhou <jinyuanzhou@sohu-inc.com>
2019-08-12 20:08:10 +0800

    tornado IOStream initialization parameters remove custom ioloop in new version('5.0.2')

--- tornadoredis/connection.py
+++ tornadoredis/connection.py
@@ -72,7 +72,7 @@ class Connection(object):
                         timeout=self.timeout
                     )
                     sock.setsockopt(socket.SOL_TCP, socket.TCP_NODELAY, 1)
-                self._stream = IOStream(sock, io_loop=self._io_loop)
+                self._stream = IOStream(sock)
                 self._stream.set_close_callback(self.on_stream_close)
                 self.info['db'] = 0
                 self.info['pass'] = None
